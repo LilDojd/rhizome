674f20fe51f296830251dde0ed34872efd0201c7
LilDojd <yawner@pm.me>
2025-06-18 19:33:23 +0400

    Start migration

---
+++ flake.nix
@@ -0,0 +1,184 @@
+{
+  nixConfig = {
+    abort-on-warn = true;
+    extra-experimental-features = [ "pipe-operators" ];
+    allow-import-from-derivation = false;
+  };
+
+  inputs.self.submodules = true;
+
+  inputs = {
+    cpu-microcodes = {
+      flake = false;
+      url = "github:platomav/CPUMicrocodes";
+    };
+
+    files.url = "github:mightyiam/files";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        flake-compat.follows = "dedupe_flake-compat";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    home-manager = {
+      url = "";
+      flake = true;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    import-tree.url = "github:vic/import-tree";
+
+    make-shell = {
+      url = "github:nicknovitski/make-shell";
+      inputs.flake-compat.follows = "dedupe_flake-compat";
+    };
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    nvf.url = "github:notashelf/nvf";
+
+    _1password-shell-plugins.url = "github:1Password/shell-plugins";
+
+    sink-rotate = {
+      url = "github:mightyiam/sink-rotate";
+      inputs = {
+        flake-parts.follows = "flake-parts";
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "dedupe_systems";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
+    swww.url = "github:LGFae/swww";
+
+    stylix = {
+      url = "github:danth/stylix";
+      flake = true;
+      inputs = {
+        flake-compat.follows = "dedupe_flake-compat";
+        flake-parts.follows = "flake-parts";
+        git-hooks.follows = "git-hooks";
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+        nur.follows = "dedupe_nur";
+        systems.follows = "dedupe_systems";
+        tinted-schemes.follows = "tinted-schemes";
+      };
+    };
+
+    tinted-schemes = {
+      flake = false;
+      url = "github:tinted-theming/schemes";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ucodenix = {
+      url = "github:e-tho/ucodenix";
+      inputs.cpu-microcodes.follows = "cpu-microcodes";
+    };
+
+  };
+
+  # _additional_ `inputs` only for deduplication
+  inputs = {
+    dedupe_flake-compat.url = "github:edolstra/flake-compat";
+
+    dedupe_flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "dedupe_systems";
+    };
+
+    dedupe_nur = {
+      url = "github:nix-community/NUR";
+      inputs = {
+        flake-parts.follows = "flake-parts";
+        nixpkgs.follows = "nixpkgs";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
+    dedupe_nuschtos-search = {
+      url = "github:NuschtOS/search";
+      inputs = {
+        flake-utils.follows = "dedupe_flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    dedupe_systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      text.readme.parts = {
+        disallow-warnings =
+          # markdown
+          ''
+            ## Trying to disallow warnings
+
+            This at the top level of the `flake.nix` file:
+
+            ```nix
+            nixConfig.abort-on-warn = true;
+            ```
+
+            > [!NOTE]
+            > It does not currently catch all warnings Nix can produce, but perhaps only evaluation warnings.
+          '';
+
+        flake-inputs-dedupe-prefix =
+          # markdown
+          ''
+            ## Flake inputs for deduplication are prefixed
+
+            Some explicit flake inputs exist solely for the purpose of deduplication.
+            They are the target of at least one `<input>.inputs.<input>.follows`.
+            But what if in the future all of those targeting `follows` are removed?
+            Ideally, Nix would detect that and warn.
+            Until that feature is available those inputs are prefixed with `dedupe_`
+            and placed in an additional separate `inputs` attribute literal
+            for easy identification.
+
+          '';
+
+        automatic-import =
+          # markdown
+          ''
+            ## Automatic import
+
+            Nix files (they're all flake-parts modules) are automatically imported.
+            Nix files prefixed with an underscore are ignored.
+            No literal path imports are used.
+            This means files can be moved around and nested in directories freely.
+
+            > [!NOTE]
+            > This pattern has been the inspiration of [an auto-imports library, import-tree](https://github.com/vic/import-tree).
+
+          '';
+      };
+
+      imports = [ (inputs.import-tree ./modules) ];
+
+      _module.args.rootPath = ./.;
+    };
+}
