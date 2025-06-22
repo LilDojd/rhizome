ec61e376a263247b0f0dc0e569a8f755831f85f6
LilDojd <yawner@pm.me>
2025-06-22 19:55:40 +0400

    Update lock

--- modules/languages/uv.nix
+++ modules/languages/uv.nix
@@ -1,15 +1,8 @@
 {
-  flake.modules =
+  flake.modules.darwin.pc =
     { pkgs, ... }:
     {
-      nixos.pc = {
-        environment.localBinInPath = true;
-        environment.variables.UV_PYTHON_DOWNLOADS = "never";
-        environment.systemPackages = with pkgs; [ uv ];
-      };
-      darwin.pc = {
-        environment.variables.UV_PYTHON_DOWNLOADS = "never";
-        environment.systemPackages = with pkgs; [ uv ];
-      };
+      environment.systemPackages = with pkgs; [ uv ];
+      environment.variables.UV_PYTHON_DOWNLOADS = "never";
     };
 }
