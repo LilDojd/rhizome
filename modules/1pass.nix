dc8a97c88900777d2f32783cb04e8d6d5bde8ed3
LilDojd <yawner@pm.me>
2025-06-22 18:35:06 +0400

    Fix

--- modules/1pass.nix
+++ modules/1pass.nix
@@ -35,7 +35,7 @@
         let
           onePassPath =
             if pkgs.stdenv.isDarwin then
-              "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+              ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"''
             else
               "~/.1password/agent.sock";
         in
@@ -43,7 +43,7 @@
           programs.ssh = {
             extraConfig = ''
               Host *
-                  IdentityAgent ${onePassPath}
+              IdentityAgent ${onePassPath}
             '';
           };
         };
