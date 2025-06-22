8a3913b3b45bd83e340177ca05791f7daabc0f8d
LilDojd <yawner@pm.me>
2025-06-22 19:14:01 +0400

    Set "hx" as editor

---
+++ modules/editors/system-env.nix
@@ -0,0 +1,11 @@
+{
+  flake.modules =
+    let
+      defaultEditor = {
+        environment.variables.EDITOR = "hx";
+      };
+    in {
+      nixos.pc = defaultEditor;
+      darwin.pc = defaultEditor;
+    };
+}
