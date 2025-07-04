a5942a44300a274cb3a2a93f11e18d4135e3452f
LilDojd <yawner@pm.me>
2025-07-04 19:00:44 +0400

    boot

---
+++ modules/boot/initd.nix
@@ -0,0 +1,1 @@
+{ flake.modules.nixos.pc.boot.initrd.systemd.enable = true; }
---
+++ modules/boot/visuals.nix
@@ -0,0 +1,10 @@
+{
+  flake.modules.nixos.pc = {
+    stylix.targets.grub.enable = false;
+
+    boot = {
+      kernelParams = [ "quiet" "systemd.show_status=error" ];
+      plymouth.enable = true;
+    };
+  };
+}
