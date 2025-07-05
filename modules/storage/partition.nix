12ee14e5d968caf3498c0c7aab07a31bbb68dcd9
LilDojd <yawner@pm.me>
2025-07-05 23:31:26 +0400

    Refactor disko

--- modules/storage/partition.nix
+++ modules/storage/partition.nix
@@ -1,21 +1,9 @@
 {
-  lib,
   inputs,
-  self,
   ...
 }:
 {
   flake.modules.nixos.pc = {
     imports = [ inputs.disko.nixosModules.disko ];
-
-    config = lib.mkMerge [
-      (import "${self}/disko/partition.nix" {
-        ssd = false;
-        ssdOptions = [ ];
-        inherit lib;
-        impermanence = true;
-        ecnrypted = false;
-      })
-    ];
   };
 }
