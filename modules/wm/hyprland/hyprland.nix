feabeae62e2171a7ce9db0df983097f5c4f3337b
LilDojd <yawner@pm.me>
2025-07-07 01:21:21 +0400

    debugging

--- modules/wm/hyprland/hyprland.nix
+++ modules/wm/hyprland/hyprland.nix
@@ -37,18 +37,6 @@
         '';
 
         settings = {
-          exec-once = [
-            "wl-paste --type text --watch cliphist store # Stores only text data"
-            "wl-paste --type image --watch cliphist store # Stores only image data"
-            "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-            "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-            "systemctl --user start hyprpolkitagent"
-            "killall -q swww;sleep .5 && swww-daemon"
-            "killall -q waybar;sleep .5 && waybar"
-            "killall -q swaync;sleep .5 && swaync"
-            "nm-applet --indicator"
-            "pypr &"
-          ];
 
           input = {
             numlock_by_default = true;
@@ -61,6 +49,8 @@
               scroll_factor = 0.8;
             };
           };
+
+          debug.disable_logs = false;
 
           gestures = {
             workspace_swipe = 1;
