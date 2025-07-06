387b1a683319bf599a3a4e20d7ca98ab8c81e896
LilDojd <yawner@pm.me>
2025-07-06 23:41:11 +0400

    fmt

--- modules/nvidia-gpu.nix
+++ modules/nvidia-gpu.nix
@@ -1,16 +1,18 @@
 {
-  flake.modules.nixos.nvidia-gpu = { config, ... }: {
-    specialisation.nvidia-gpu.configuration = {
-      services.xserver.videoDrivers = [ "nvidia" ];
-      hardware.nvidia = {
-        modesetting.enable = true;
-        powerManagement.enable = false;
-        powerManagement.finegrained = false;
-        nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.latest;
+  flake.modules.nixos.nvidia-gpu =
+    { config, ... }:
+    {
+      specialisation.nvidia-gpu.configuration = {
+        services.xserver.videoDrivers = [ "nvidia" ];
+        hardware.nvidia = {
+          modesetting.enable = true;
+          powerManagement.enable = false;
+          powerManagement.finegrained = false;
+          nvidiaSettings = true;
+          package = config.boot.kernelPackages.nvidiaPackages.latest;
+        };
       };
     };
-  };
   nixpkgs.allowedUnfreePackages = [
     "nvidia-x11"
     "nvidia-settings"
