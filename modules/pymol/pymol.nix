c5a8394d7d4c264d89e281d9ac4eb781c116c81e
LilDojd <yawner@pm.me>
2025-07-08 00:44:37 +0400

    Attempt to fix pymol

---
+++ modules/pymol/pymol.nix
@@ -0,0 +1,35 @@
+{
+  flake.modules = {
+    homeManager.gui =
+      { pkgs, ... }:
+      let
+        python = pkgs.python311;
+        pythonPackages = python.pkgs;
+
+        pymolFixed = pkgs.callPackage ./_pymol.nix {
+          inherit (pkgs)
+            stdenv
+            lib
+            fetchFromGitHub
+            makeDesktopItem
+            cmake
+            netcdf
+            glew
+            glm
+            libpng
+            libxml2
+            freetype
+            mmtf-cpp
+            msgpack
+            qt5
+            ;
+          python3Packages = pythonPackages;
+        };
+      in
+      {
+        home.packages = [
+          pymolFixed
+        ];
+      };
+  };
+}
