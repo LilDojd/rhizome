4702111c9cd93e147654e0fb63f5484ae5dd7b4b
LilDojd <yawner@pm.me>
2025-07-07 23:38:35 +0400

    Patch slack desktop

--- modules/slack.nix
+++ modules/slack.nix
@@ -1,10 +1,34 @@
-_: {
+{
   flake.modules = {
-    nixos.foundation =
+    homeManager.gui =
       { pkgs, ... }:
       {
-        environment.systemPackages = with pkgs; [ slack ];
+        home.packages = with pkgs; [
+          slack
+        ];
+
       };
+    homeManager.hyprland = {
+      xdg.desktopEntries = {
+        slack = {
+          name = "Slack";
+          genericName = "Slack Client for Linux";
+          exec = "slack --use-gl=desktop + --waylandFlags -s %U";
+          terminal = false;
+          categories = [
+            "GNOME"
+            "GTK"
+            "Network"
+            "InstantMessaging"
+          ];
+
+          mimeType = [ "x-scheme-handler/slack" ];
+          icon = "slack";
+          startupNotify = true;
+          type = "Application";
+        };
+      };
+    };
   };
   nixpkgs.allowedUnfreePackages = [ "slack" ];
 }
