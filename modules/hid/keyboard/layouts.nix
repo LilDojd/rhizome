5f352b0740505f82e6b12fef4cf5048432984a53
LilDojd <yawner@pm.me>
2025-07-07 18:34:49 +0400

    Remove i3bars

--- modules/hid/keyboard/layouts.nix
+++ modules/hid/keyboard/layouts.nix
@@ -48,30 +48,5 @@
           input.kb_options = [ "grp:ctrl_space_toggle" ];
         };
       };
-
-      programs.i3status-rust.bars.bottom.blocks = lib.mkOrder 1200 [
-        {
-          block = "keyboard_layout";
-          if_command = pkgs.writeShellScript "in-sway" ''
-            [ "$XDG_CURRENT_DESKTOP" = "sway" ]
-          '';
-          driver = "sway";
-          mappings = {
-            "English (US)" = "EN";
-            "Russian" = "RU";
-          };
-        }
-        {
-          block = "custom";
-          if_command = pkgs.writeShellScript "in-hyprland" ''
-            [ "$XDG_CURRENT_DESKTOP" = "Hyprland" ]
-          '';
-          watch_files = [ hyprlandStateFile ];
-          interval = "once";
-          command =
-            withSystem pkgs.system (psArgs: psArgs.config.packages.get-hyprland-main-keyboard-layout)
-            |> lib.getExe;
-        }
-      ];
     };
 }
