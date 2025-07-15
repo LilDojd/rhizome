9776ee5e4b1be2d79213696b2edb104129cebf5f
LilDojd <yawner@pm.me>
2025-07-15 21:12:34 +0400

    Resolve studio

--- modules/davinci.nix
+++ modules/davinci.nix
@@ -1,8 +1,8 @@
 {
-
+  nixpkgs.allowedUnfreePackages = [ "davinci-resolve-studio" ];
   flake.modules.nixos.foundation =
     { pkgs, ... }:
     {
-      environment.systemPackages = [ pkgs.davinci-resolve ];
+      environment.systemPackages = [ pkgs.davinci-resolve-studio ];
     };
 }
