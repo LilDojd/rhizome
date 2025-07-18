f4e4eb44cb15f239c71431254aaac65b12317df2
LilDojd <yawner@pm.me>
2025-07-18 18:48:55 +0400

    Networking

--- modules/networking/manager.nix
+++ modules/networking/manager.nix
@@ -8,8 +8,21 @@
             IPv6.Enabled = true;
             Settings.AutoConnect = true;
           };
+
         };
-        networkmanager.wifi.backend = "iwd";
+        useDHCP = false;
+        dhcpcd.enable = false;
+        nameservers = [
+          "1.1.1.1"
+          "1.0.0.1"
+          "8.8.8.8"
+          "8.8.4.4"
+        ];
+        networkmanager = {
+          wifi.backend = "iwd";
+          enable = true;
+          dns = "none";
+        };
       };
     };
 
