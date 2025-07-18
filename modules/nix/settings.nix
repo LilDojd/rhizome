1f501946c6473c96d92179b64062ecf4a77f968a
LilDojd <yawner@pm.me>
2025-07-18 19:32:53 +0400

    Small fixes

--- modules/nix/settings.nix
+++ modules/nix/settings.nix
@@ -12,12 +12,13 @@
       ];
       extra-system-features = [ "recursive-nix" ];
       # export NIX_CONFIG="substituters = https://cache.nixos.org https://hyprland.cachix.org\ntrusted-public-keys = hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      substituters = [
+      extra-substituters = [
         "https://cache.nixos.org"
         "https://hyprland.cachix.org"
         "https://helix.cachix.org"
+
       ];
-      trusted-public-keys = [
+      extra-trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
