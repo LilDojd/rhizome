9a128a2a978911dd3c8b55fe2c01015e6c473f1d
LilDojd <yawner@pm.me>
2025-07-23 14:19:53 +0400

    Fix typo

--- modules/wm/hyprland/free-buffer.nix
+++ modules/wm/hyprland/free-buffer.nix
@@ -35,7 +35,7 @@ let
   };
 in
 {
-  flake.modules.homeManager.hyprland = {
+  flake.modules.nixos.foundation = {
     environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".text =
       limitFreeBufferProfile;
   };
