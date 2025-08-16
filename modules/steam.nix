b84c713300bf9b9f265e078511e9a285e36731ef
LilDojd <yawner@pm.me>
2025-08-16 18:48:04 +0400

    Add steam

---
+++ modules/steam.nix
@@ -0,0 +1,29 @@
+{
+  flake.modules.homeManager.base =
+    { config, ... }:
+    {
+      home.sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATH = "${config.home.homeDirectory}/.steam/root/compatibilitytools.d";
+    };
+  flake.modules.nixos.foundation =
+    { pkgs, ... }:
+    {
+      programs.steam = {
+        enable = true;
+        gamescopeSession.enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+      };
+      programs.gamemode.enable = true;
+      environment.systemPackages = with pkgs; [
+        mangohud
+        protonup
+      ];
+    };
+  nixpkgs.allowedUnfreePackages = [
+    "steam"
+    "steam-original"
+    "steam-unwrapped"
+    "steam-run"
+  ];
+}
