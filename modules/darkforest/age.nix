24cf42fde080e494ff4ce0c8d76690ded82ce6ca
LilDojd <yawner@pm.me>
2025-08-28 02:00:26 +0400

    Update ssh host key and rekey

--- modules/darkforest/age.nix
+++ modules/darkforest/age.nix
@@ -1,4 +1,4 @@
 {
   flake.modules.nixos."nixosConfigurations/darkforest".age.rekey.hostPubkey =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIw3Icl0DBS0UCApKcUkcj6aBu5mrUvXeZMEbi+djh0k";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOR+kt3xFzKOD/EU9uT/qiECS1L7FCyDeHSucKM7gkQN";
 }
