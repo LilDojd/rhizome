69e65f7cefe3f7460f82a92860f2fde1fb0fbf8f
LilDojd <yawner@pm.me>
2025-08-29 15:18:07 +0400

    Add jj.yazi from my frok

--- modules/file-management/yazi/base.nix
+++ modules/file-management/yazi/base.nix
@@ -1,6 +1,34 @@
-_: {
+{ lib, ... }:
+{
   flake.modules.homeManager.base =
     { pkgs, ... }:
+    let
+      jj = pkgs.yaziPlugins.mkYaziPlugin {
+        pname = "jj.yazi";
+        version = "25.5.31-unstable-2025-07-05";
+
+        installPhase = ''
+          runHook preInstall
+          cp -r jj.yazi $out
+          rm $out/LICENSE
+          cp LICENSE $out
+          runHook postInstall
+        '';
+
+        src = pkgs.fetchFromGitHub {
+          owner = "LilDojd";
+          repo = "plugins";
+          rev = "1d79d36941cc1e92bdce2300bff597c7a6fa42b4";
+          hash = "sha256-Z8nBzjglGZRRkzyBb+CeaYKKRudE30bhrjWpWVC1mN4=";
+        };
+
+        meta = {
+          description = "Show the status of Jujutsu file changes as linemode in the file list";
+          license = lib.licenses.mit;
+          maintainers = with lib.maintainers; [ LilDojd ];
+        };
+      };
+    in
     {
       home.packages = with pkgs; [
         atool
@@ -20,6 +48,7 @@ _: {
             inherit (pkgs.yaziPlugins) lazygit;
             inherit (pkgs.yaziPlugins) full-border;
             inherit (pkgs.yaziPlugins) smart-enter;
+            inherit jj;
           };
           initLua = ''
             			require("full-border"):setup()
