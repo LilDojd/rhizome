e997beda9f2d077625a6aead4146e120d4fac61b
LilDojd <yawner@pm.me>
2025-08-31 16:51:48 +0400

    Nvidia cachix

--- modules/nvidia-gpu.nix
+++ modules/nvidia-gpu.nix
@@ -10,9 +10,17 @@
         nvidiaSettings = true;
         package = config.boot.kernelPackages.nvidiaPackages.latest;
       };
+      nix.settings = {
+        extra-substituters = [ "https://cuda-maintainers.cachix.org" ];
+        extra-trusted-public-keys = [
+          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        ];
+      };
     };
+
   nixpkgs.allowedUnfreePackages = [
     "nvidia-x11"
     "nvidia-settings"
   ];
+
 }
