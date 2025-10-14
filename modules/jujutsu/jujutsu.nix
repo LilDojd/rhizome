5d19c9a2a1c8e6c8237037e76705e1e56892800b
LilDojd <yawner@pm.me>
2025-10-14 17:46:37 +0400

    Add signed-off trailer

--- modules/jujutsu/jujutsu.nix
+++ modules/jujutsu/jujutsu.nix
@@ -27,6 +27,11 @@
             backend = "ssh";
             key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESOx5jXSV+jeGmIpVO3ASIByLflNIhnkfAlmXOnMsXk";
           };
+          templates = {
+            commit_trailers = ''
+              format_signed_off_by_trailer(self)
+              ++ if(!trailers.contains_key("Change-Id"), format_gerrit_change_id_trailer(self))'';
+          };
           git.sign-on-push = true;
           ui.show-cryptographic-signatures = true;
           ui.editor = lib.mkIf homeArgs.config.programs.helix.enable "hx";
