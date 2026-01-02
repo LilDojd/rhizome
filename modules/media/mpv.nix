b1e3e597d98d546b16ff4dfc2356e017e614a497
LilDojd <yawner@pm.me>
2026-01-03 03:36:09 +0400

    chore: tweaks
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: Idc7f443729e0cfd23b4312609fb3567b6a6a6964

--- modules/media/mpv.nix
+++ modules/media/mpv.nix
@@ -1,24 +1,23 @@
 {
   flake.modules.homeManager.linux =
     { pkgs, ... }:
+    let
+      mpv-wayland = pkgs.mpv.override {
+        scripts = with pkgs.mpvScripts; [
+          uosc
+          sponsorblock
+        ];
+
+      };
+    in
     {
       programs.mpv = {
         enable = true;
 
-        package = (
-          pkgs.mpv-unwrapped.wrapper {
-            scripts = with pkgs.mpvScripts; [
-              uosc
-              sponsorblock
-            ];
-
-            mpv = pkgs.mpv-unwrapped.override {
-              waylandSupport = true;
-            };
-          }
-        );
+        package = mpv-wayland;
 
         config = {
+          gpu-context = "wayland";
           profile = "high-quality";
           ytdl-format = "bestvideo+bestaudio";
           cache-default = 4000000;
--- modules/mineral.nix
+++ modules/mineral.nix
@@ -3,7 +3,31 @@
   flake.modules = {
     nixos.foundation = {
       imports = [ "${inputs.nix-mineral}/nix-mineral.nix" ];
-      nix-mineral.enable = true;
+      nix-mineral = {
+        enable = true;
+        settings = {
+          kernel = {
+            # if false, may prevent low resource systems from booting.
+            busmaster-bit = true;
+
+            # Enable symmetric multithreading and just use default CPU mitigations,
+            # to potentially improve performance.
+            # DO NOT disable all cpu mitigations,
+            cpu-mitigations = "smt-on";
+
+            # Could increase I/O performance on ARM64 systems, with risk.
+            iommu-passthrough = true;
+
+            # PTI (Page Table Isolation) may tax performance.
+            pti = false;
+          };
+          system.multilib = true;
+          network = {
+            ip-forwarding = true;
+          };
+        };
+
+      };
     };
   };
 }
