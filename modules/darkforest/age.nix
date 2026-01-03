6d9bef37a147659196a5746037deac6b9d574f1a
LilDojd <yawner@pm.me>
2026-01-03 06:12:24 +0400

    chore: rekey
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: I5bd1c26e597fcfef7cfbdcf5de2c068d6a6a6964

--- modules/darkforest/age.nix
+++ modules/darkforest/age.nix
@@ -1,4 +1,4 @@
 {
   flake.modules.nixos."nixosConfigurations/darkforest".age.rekey.hostPubkey =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJRtlUmODxrD2dEUr6wdkXAtuSl170nHBA2YsUKrO6b3";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJdlkG7kiXgM8vDgt5eqVjkKZRgbZE0XetkzarsFKzxS";
 }
