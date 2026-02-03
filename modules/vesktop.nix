a6915288d1bf239d3d3afaebbaea104933d99d33
LilDojd <yawner@pm.me>
2026-02-03 13:25:18 +0400

    chore: use overlay for vesktop
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: I916d3a246b1357ae1a7aba3c62de02c66a6a6964

--- modules/vesktop.nix
+++ modules/vesktop.nix
@@ -1,3 +1,22 @@
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      vesktop = prev.vesktop.overrideAttrs (old: {
+        buildPhase = ''
+          runHook preBuild
+
+          pnpm build
+          pnpm exec electron-builder \
+            --dir \
+            -c.asarUnpack="**/*.node" \
+            -c.electronDist=${if prev.stdenv.hostPlatform.isDarwin then "." else "electron-dist"} \
+            -c.electronVersion=${prev.electron.version} \
+            ${if prev.stdenv.hostPlatform.isDarwin then "-c.mac.identity=null" else ""}
+
+          runHook postBuild
+        '';
+      });
+    })
+  ];
   flake.modules.homeManager.gui.programs.vesktop.enable = true;
 }
