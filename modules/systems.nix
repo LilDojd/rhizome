f1996e24747018dff3a7fb0db8b3f851d64fd29d
LilDojd <yawner@pm.me>
2026-03-23 23:14:36 +0400

    chore: exclude aarch64-linux
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: Ib4a75935189476d6dfd5eea3efe48a7a6a6a6964

--- modules/systems.nix
+++ modules/systems.nix
@@ -1,4 +1,10 @@
 { inputs, lib, ... }:
+let
+  excluded = [
+    "x86_64-darwin"
+    "aarch64-linux"
+  ];
+in
 {
-  systems = import inputs.systems |> lib.filter (s: s != "x86_64-darwin");
+  systems = import inputs.systems |> lib.filter (s: !(lib.elem s excluded));
 }
