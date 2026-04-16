d89d277ab5d07c8fc6bb6cb5c08de9fa59c47007
LilDojd <yawner@pm.me>
2026-04-16 18:08:59 +0400

    chore: plymouth visuals
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: I8ea7b86b8353b04f6cb0fbdd7c2046986a6a6964

--- modules/boot/visuals.nix
+++ modules/boot/visuals.nix
@@ -3,8 +3,11 @@
     stylix.targets.grub.enable = false;
 
     boot = {
+      consoleLogLevel = 3;
+      initrd.verbose = false;
       kernelParams = [
         "quiet"
+        "udev.log_level=3"
         "systemd.show_status=error"
       ];
       plymouth.enable = true;
