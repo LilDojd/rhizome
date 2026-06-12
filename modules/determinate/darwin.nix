28cbe5d61ed92687090f6f21dff093fa4b1d14d5
LilDojd <yawner@pm.me>
2026-06-12 13:51:01 +0400

    refactor: dedupe nix-community cache settings
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: Ic0545e4ad25fb159149def0e5cf51a786a6a6964

--- modules/determinate/darwin.nix
+++ modules/determinate/darwin.nix
@@ -5,8 +5,6 @@
     environment.systemPackages = [ inputs.fh.packages."aarch64-darwin".default ];
     nix.enable = false;
     determinateNix.customSettings = config.nix.settings // {
-      substituters = [ "https://nix-community.cachix.org" ];
-      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
       trusted-users = [ config.flake.meta.yawner.username ];
     };
   };
--- modules/nix/settings.nix
+++ modules/nix/settings.nix
@@ -17,11 +17,13 @@
         "https://cache.nixos.org"
         "https://hyprland.cachix.org"
         "https://helix.cachix.org"
+        "https://nix-community.cachix.org"
         "https://install.determinate.systems"
       ];
       extra-trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       ];
