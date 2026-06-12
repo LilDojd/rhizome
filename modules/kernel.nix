60ef47288327ae4711ec5c202ecab712d4530515
LilDojd <yawner@pm.me>
2026-06-12 13:40:29 +0400

    chore: pin kernel to 7.0.11
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: Icd59f40e3bb870713c692ee56a6edff46a6a6964

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -2,7 +2,18 @@
   flake.modules.nixos.foundation =
     { pkgs, ... }:
     {
-      boot.kernelPackages = pkgs.linuxPackages_latest;
+      boot.kernelPackages = pkgs.linuxPackagesFor (
+        pkgs.linux_latest.override {
+          argsOverride = rec {
+            version = "7.0.11";
+            modDirVersion = version;
+            src = pkgs.fetchurl {
+              url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
+              hash = "sha256-5WyDVt2gETamBBxu+DK9Dsmb0tNd/5eDKqXsEO0BQwQ=";
+            };
+          };
+        }
+      );
       boot.kernel.sysctl = {
         "net.core.default_qdisc" = "fq";
         "net.ipv4.tcp_congestion_control" = "bbr";
