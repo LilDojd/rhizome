d434353388f10b06f1bbaf61615e69012a7908b7
LilDojd <yawner@pm.me>
2026-06-12 14:36:12 +0400

    revert: unpin linux kernel
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: I3b319a76342421be96303166907693f86a6a6964

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -2,18 +2,7 @@
   flake.modules.nixos.foundation =
     { pkgs, ... }:
     {
-      boot.kernelPackages = pkgs.linuxPackagesFor (
-        pkgs.linux_latest.override {
-          argsOverride = rec {
-            version = "7.0.11";
-            modDirVersion = version;
-            src = pkgs.fetchurl {
-              url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
-              hash = "sha256-5WyDVt2gETamBBxu+DK9Dsmb0tNd/5eDKqXsEO0BQwQ=";
-            };
-          };
-        }
-      );
+      boot.kernelPackages = pkgs.linuxPackages_latest;
       boot.kernel.sysctl = {
         "net.core.default_qdisc" = "fq";
         "net.ipv4.tcp_congestion_control" = "bbr";
