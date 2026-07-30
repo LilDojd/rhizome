47f7bd2fd0a3d789ad5a902476e3c54d1e0d6e73
LilDojd <yawner@pm.me>
2026-07-30 16:07:08 +0400

    chore: remove a todo
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: I34f8f61e92bd7744a317b32e21d04c1a6a6a6964

--- modules/vesktop.nix
+++ modules/vesktop.nix
@@ -6,9 +6,4 @@
       ".config/vesktop"
     ];
   flake.modules.homeManager.gui.programs.vesktop.enable = true;
-  # TODO: remove electron-40.10.5 after https://github.com/NixOS/nixpkgs/issues/542512 resolved
-  nixpkgs.config.permittedInsecurePackages = [
-    "pnpm-10.29.2"
-    "electron-40.10.5"
-  ];
 }
