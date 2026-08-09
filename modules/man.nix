cc70fef66d8f9dad9504e7f0a0023ec7c05b07d9
LilDojd <yawner@pm.me>
2026-08-09 16:46:30 +0400

    feat: developer man pages
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: Iaac02d4aef6b2936e51d975c785c4d656a6a6964

---
+++ modules/man.nix
@@ -0,0 +1,11 @@
+{
+  flake.modules.nixos.foundation =
+    { pkgs, ... }:
+    {
+      documentation.dev.enable = true;
+      environment.systemPackages = with pkgs; [
+        man-pages
+        man-pages-posix
+      ];
+    };
+}
