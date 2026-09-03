01596353399a399240bdcfd71e06450551eb5062
LilDojd <yawner@pm.me>
2026-09-03 12:33:13 +0400

    fix: fix pymol build
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: I7234967af5438d26e0778284997bc0b16a6a6964

--- modules/pymol/pymol.nix
+++ modules/pymol/pymol.nix
@@ -1,9 +1,23 @@
 {
   flake.modules.homeManager.gui =
     { pkgs, ... }:
+    let
+      # PyQt5 omits the ABI declaration required by SIP 6.16.
+      python3Packages = pkgs.python3Packages.overrideScope (
+        _: previous: {
+          pyqt5 = previous.pyqt5.overridePythonAttrs (old: {
+            postPatch = old.postPatch + ''
+              substituteInPlace sip/QtCore/QtCoremod.sip \
+                --replace-fail '%Plugin PyQt5' \
+                $'%Plugin PyQt5\n%MinimumABIVersion "12.13"'
+            '';
+          });
+        }
+      );
+    in
     {
       home.packages = [
-        pkgs.pymol
+        (pkgs.pymol.override { inherit python3Packages; })
       ];
     };
 }
